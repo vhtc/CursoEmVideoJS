b8393716edfdc33f8d08aaf1e8572b6d37820afb
Victor Tuy <31480086+vhtc@users.noreply.github.com>
2024-06-25 13:52:59 -0300

    Atualizado botão premier + js do painelTV

---
+++ exTV/appTV.js
@@ -0,0 +1,7 @@
+function premiere(id) {
+    var src = 'src';
+    console.log(id)
+
+    var painelTV = window.document.getElementById('iframeTv')
+    painelTV.setAttribute(src, `https://xn----------------g34l3fkp7msh1cj3acobj33ac2a7a8lufomma7cf2b1sh.xn---1l1--5o4dxb.xn---22--11--33--99--75---------b25zjf3lta6mwf6a47dza94e.xn--pck.xn--zck.xn--0ck.xn--pck.xn--yck.xn-----0b4asja8cbew2b4b0gd0edbjm2jpa1b1e9zva7a0347s4da2797e8qri.xn--1ck2e1b/player3/ch.php?canal=${id}`)
+}
