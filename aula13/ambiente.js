b4ad7cdab5e73dc04b05135e2f0df041c6d77600
Victor Tuy <31480086+vhtc@users.noreply.github.com>
2024-06-28 07:49:15 -0300

    While and for

---
+++ aula13/ambiente.js
@@ -0,0 +1,14 @@
+var cont = 1
+var con = 1
+console.log('Primeiro Teste')
+while (cont <= 6) {
+    console.log(`Tudo Bem ${cont}`)
+    cont++
+}
+
+console.log('Segundo Teste')
+
+do {
+    console.log(`Tudo Bem ${con}`)
+    con++
+} while (con <= 6)
