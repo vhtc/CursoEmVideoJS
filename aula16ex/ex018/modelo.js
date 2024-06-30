41db1cfd2f6fecd985fb874b920e0e70a6530efe
Victor Tuy <victor.tuy@hotmail.com>
2024-06-30 15:23:25 -0300

    Lista estrutura de dados

---
+++ aula16ex/ex018/modelo.js
@@ -0,0 +1,38 @@
+var lista = []
+
+function inserirLista() {
+    let numero = Number(document.getElementById('inserirNumero').value)
+    let tamanho = document.getElementById('inserirNumero').value
+    let selectLista = document.getElementById('listaNumeros')
+    let resultado = document.getElementById('resultado')
+    var item = document.createElement('option')
+
+    if (tamanho.length == 0) {
+        alert('[ERRO] Digite um valor válido!')
+    } else if (lista.indexOf(numero) == -1) {
+        lista.push(numero)
+        resultado.innerHTML = `<p>${lista}</p>`
+        selectLista.appendChild(item)
+        item.text=`Adicionado ${numero}`
+
+    } else {
+        alert('[ERRO] Valor já existe na lista!')
+    }
+    window.document.getElementById('crescenteLista').disabled = false
+}
+
+function limparLista(){
+    let selectLista = document.getElementById('listaNumeros')
+    let resultado = document.getElementById('resultado')
+    selectLista.innerHTML = ''
+    lista = []
+    resultado.innerHTML = ''
+}
+
+function crescenteLista() {
+    let resultado = document.getElementById('resultado')
+    lista.sort()
+    resultado.innerHTML = `<p>${lista}</p>`
+    window.document.getElementById('crescenteLista').disabled = true
+    alert('Lista atualizada')
+}
